2281e3d265dac89357a54a07741d30da2b8345f1
Zagidullin-Bulat <120476991+Zagidullin-Bulat@users.noreply.github.com>
2023-01-15 18:44:46 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Выполнить с помощью рекурсии.
+
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+
+if (m > n)
+    Console.WriteLine("Ошибка, введите верные данные.");
+    
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+        
+    return n + CountNaturalSum(m, n - 1);
+}
+
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
